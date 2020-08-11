4c82c3b231a0c9b511d3f8bfb43e229b1910b625
shine <shine130@vip.qq.com>
2020-08-11 10:47:02 +0800

    Hash 用户密码Bcrypt:

--- src/app/user/user.middleware.js
+++ src/app/user/user.middleware.js
@@ -1,4 +1,5 @@
 const userService = require("./user.service");
+const bcrypt = require("bcrypt");
 
 // 验证用户数据
 
@@ -10,15 +11,26 @@ const validateUserData = async (req, res, next) => {
   //验证必填数据
   if (!name) return next(new Error("NAME_IS_REQUIRED"));
   if (!password) return next(new Error("PASSWORD_IS_REQUIRED"));
-  
+
   // 验证用户名
   const user = await userService.getUserByName(name);
-  if(user) return next(new Error('USER_ALREADY_EXIST'));
+  if (user) return next(new Error("USER_ALREADY_EXIST"));
 
+  //下一步
+  next();
+};
+
+// HASH密码
+
+const hashPassword = async (req, res, next) => {
+  const { password } = req.body;
+  // HASH密码
+  req.body.password = await bcrypt.hash(password, 10);
   //下一步
   next();
 };
 
 module.exports = {
   validateUserData,
+  hashPassword,
 };
--- src/app/user/user.router.js
+++ src/app/user/user.router.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const userController = require('./user.controller');
-const {validateUserData} = require('./user.middleware');
+const express = require("express");
+const userController = require("./user.controller");
+const { validateUserData, hashPassword } = require("./user.middleware");
 
 const router = express.Router();
 
 // 创建用户
 
-router.post('/users',validateUserData,userController.store)
+router.post("/users", validateUserData,hashPassword, userController.store);
 
 // 导出路由
 
 module.exports = {
   router,
-}
+};
